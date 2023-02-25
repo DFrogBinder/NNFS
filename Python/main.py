3f65c7123c81b48f96bb7e3269104ec0ecf32dee
Boyan Ivanov <boyan.ivanov@perspectum.com>
2023-02-25 13:36:27 +0000

    categorical cross-entropy

--- Python/main.py
+++ Python/main.py
@@ -20,11 +20,62 @@ class Layer_Dense:
 class Activation_ReLU:
     def forward(self, inputs):
         self.output = np.maximum(0, inputs)
+class Activation_Softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
 
-layer1 = Layer_Dense(4,5) # Initializes a Layer_Dense object with 4 inputs and 5 neurons (This calls the __init__ method)
-layer2 = Layer_Dense(5,2)
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
 
-layer1.forward(X) # Pass data through Layer_Dens object
-# print(layer1.output)
-layer2.forward(layer1.output)
-print(layer2.output)
+        self.output = probabilities 
+class Loss:
+    # calculate the data and the regularization loss,
+    # given model output and ground truth values
+    def calculate(self, output,y):
+        # Calculate the sample loss
+        sample_losses = self.forward(output,y)
+
+        # Calculate the mean loss
+        data_loss = np.mean(sample_losses)
+
+        return data_loss
+
+# Cross-entropy loss computation
+class Loss_CategoricalCrossEntropy(Loss):
+    # Forward Pass
+    def forward(self,y_pred,y_true):
+        #Get number of samples in a batch 
+        samples = len(y_pred)
+
+        # Clip data to prevent diviion by zero
+        y_pred_clipped = np.clip(y_pred,1e-7, 1- 1e-7)
+        # Probablities for target values
+        # only if categorial labels
+        if len(y_true.shape) == 1:
+            correct_confidence = y_pred_clipped[
+                range(samples),
+                y_true]
+        # Mask values for one-hot encoded labels
+        elif len(y_true.shape) == 2:
+            correct_confidence = np.sum(
+                y_pred_clipped * y_true, axis=1
+            )
+        # Losses
+        negative_log_likelihood = -np.log(correct_confidence)
+        return negative_log_likelihood
+
+
+dense1 = Layer_Dense(2,3) # Initializes a Layer_Dense object with 4 inputs and 5 neurons (This calls the __init__ method)
+dense2 = Layer_Dense(3,3)
+
+activation1 = Activation_ReLU()
+activation2 = Activation_Softmax()
+
+loss_function = Loss_CategoricalCrossEntropy()
+
+dense1.forward(X)
+activation1.forward(dense1.output)
+
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+
+loss = loss_function.calculate(activation2.output,y)
